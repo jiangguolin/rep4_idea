3c3e9a6fcdcd4932ca26701db153fb232f235532
zs <zs@qq.com>
2020-03-21 10:52:14 +0800

    将IDEA工程添加到本地仓库

---
+++ java/src/com/jiang/git/D1.java
@@ -0,0 +1,21 @@
+package com.jiang.git;
+
+
+import java.util.Scanner;
+
+public class D1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入一个数：");
+        int n1 = input.nextInt();
+        System.out.println("输入的这个数是："+n1);
+
+        System.out.println("输入完成");
+        System.out.println("输入完成");
+        System.out.println("输入完成");
+        System.out.println("输入完成");
+        System.out.println("输入完成");
+    }
+}
